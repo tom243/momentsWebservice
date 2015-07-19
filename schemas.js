09abddbc749eb714c13d2becb169647012f40c10
tom243 <tomer.luster@gmail.com>
2015-07-19 15:23:07 +0300

    delete nodemodels

---
+++ schemas.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var album_schema = new schema({
+	album_name : {
+		type : String,
+		required : true,
+		index : 1,
+		unique : true
+	},
+	creator_name:{
+		type : String,
+		required : true,
+		index : 1,
+		unique : true
+	},
+	date : String,
+	persons : [String],
+	pic : String,
+	creation_address : String,
+	moment_event : [{
+		moment_input : String,
+		moment_type : String,
+		creation_time : String,
+		owner : String,
+		moment_latitude : Number,
+		moment_longitude : Number
+	}],
+	mobile_event:Boolean
+}, {
+	collection : 'Albums'
+});
+
+
+exports.album_schema = album_schema;
+
+var contact_schema = new schema({
+	name : {
+		type : String,
+		required : true,
+		index : 1,
+		unique : true
+	}
+}, {
+	collection : 'Users'
+});
+
+exports.contact_schema = contact_schema; 
